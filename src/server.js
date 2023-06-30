c2c3e6fad5807dd0747b86ba551fd6b0d746cc30
Matt-Bruce111 <88635981+Matt-Bruce111@users.noreply.github.com>
2023-06-30 16:27:04 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // Import express and any required modules
 const express = require('express');
+const cors = require('cors')
 // Import our db from our models folder.
 const db = require('./models');
 // Bring in our config file.
@@ -21,12 +22,18 @@ app.use(express.json({ limit: '10mb'}));
 app.use(express.urlencoded({ extended: true }));
 // Increase the limit of data that can be sent to the server.
 
+app.use(cors())
+
 // Use our routes
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 app.use('/api/services', services);
 app.use('/api/feedback', feedback);
 app.use('/api/blog', blog);
+
+app.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+})
 
 // Start the server with sequelize
 db.sequelize.sync().then(() => {
